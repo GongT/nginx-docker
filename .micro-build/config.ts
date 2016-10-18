57562bda8537724765fd6de94962511074b3925c
GongT <gongteng524702837@gmail.com>
2016-10-18 15:21:55 +0800

    fix volume

--- .micro-build/config.ts
+++ .micro-build/config.ts
@@ -22,16 +22,16 @@ build.startupCommand('nginx -c /etc/nginx/nginx.conf -g "daemon off;"');
 build.shellCommand('/bin/sh', '-c');
 build.stopCommand('nginx', '-s', 'quit');
 
-build.dependService('microservice-dnsmasq', 'git@github.com:GongT/microservice-dnsmasq.git');
-build.dockerRunArgument('--dns=127.0.0.1', '--tmpfs=/etc/nginx/generated.d:rw,noexec,nosuid,nodev,size=500k');
+build.dependService('microservice-dnsmasq', 'http://github.com/GongT/microservice-dnsmasq.git');
+build.dockerRunArgument('--dns=${HOST_LOOP_IP}', '--tmpfs=/etc/nginx/generated.d:rw,noexec,nosuid,nodev,size=500k');
 
 build.nsgLabel(ELabelNames.alias, ['nginx']);
 
 build.environmentVariable('RUN_IN_DOCKER', 'yes');
 
 build.volume('./config', '/etc/nginx');
-build.volume('/var/log', '/var/log');
-build.volume('/var/run', '/host/var/log');
+build.volume('/var/log', '/host/var/log');
+build.volume('/var/run', '/host/var/run');
 
 // build.prependDockerFile('install.Dockerfile');
 // build.appendDockerFile('configure.Dockerfile');
