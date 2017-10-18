de35496f7cf100f9667bc854ef2ded4919030ebf
GongT <admin@gongt.me>
2017-10-18 13:05:46 +0800

    add create link

--- create-links.sh
+++ create-links.sh
@@ -4,8 +4,10 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 mkdir -p certbot-root letsencrypt config
 mkdir -p /data/docker/nginx/
 
-ln -s `pwd`/certbot-root /data/docker/nginx/
-ln -s `pwd`/letsencrypt /data/docker/nginx/
-ln -s `pwd`/config /data/docker/nginx/
+for i in "certbot-root" "letsencrypt" "config"; do
+	[ -e "/data/docker/nginx/$i" ] && unlink "/data/docker/nginx/$i"
+	ln -s "`pwd`/$i" /data/docker/nginx/
+done
 
+[ -e "/etc/letsencrypt" ] && unlink "/etc/letsencrypt"
 ln -s `pwd`/letsencrypt /etc/
